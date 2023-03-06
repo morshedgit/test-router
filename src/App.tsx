1c13afa69b8a2415a6b900669de99119e8dc512b
Sadeq Morshedloo <sadeq.morshedloo@campus-support.ca>
2023-03-06 19:25:01 +0000

    Adds basename to route config

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,37 @@
 import "./App.css";
 import { createBrowserRouter, Link, RouterProvider } from "react-router-dom";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: (
+        <div>
+          <nav>
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+          </nav>
+          Home
+        </div>
+      ),
+    },
+    {
+      path: "/about",
+      element: (
+        <div>
+          <nav>
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+          </nav>
+          About
+        </div>
+      ),
+    },
+  ],
   {
-    path: "/",
-    element: (
-      <div>
-        <nav>
-          <Link to="/">Home</Link>
-          <Link to="/about">About</Link>
-        </nav>
-        Home
-      </div>
-    ),
-  },
-  {
-    path: "/about",
-    element: (
-      <div>
-        <nav>
-          <Link to="/">Home</Link>
-          <Link to="/about">About</Link>
-        </nav>
-        About
-      </div>
-    ),
-  },
-]);
+    basename: "/test-router",
+  }
+);
 
 function App() {
   return <RouterProvider router={router} />;

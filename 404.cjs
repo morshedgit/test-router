2c4a21641f75a6bfff53f7b3be6656e48ab74d8b
Sadeq Morshedloo <sadeq.morshedloo@campus-support.ca>
2023-03-06 19:46:54 +0000

    fix

--- 404.cjs
+++ 404.cjs
@@ -1,16 +1,17 @@
 "use strict";
 exports.__esModule = true;
 var fs = require("fs");
-var htmlContent = "\n  <!DOCTYPE html>\n  <html>\n    <head>\n      <title>404 Not Found</title>\n    </head>\n    <body>\n      <script>\n        window.location.href = '/';\n      </script>\n    </body>\n  </html>\n";
+var htmlContent =
+  "\n  <!DOCTYPE html>\n  <html>\n    <head>\n      <title>404 Not Found</title>\n    </head>\n    <body>\n      <script>\n        window.location.href = '/test-router';\n      </script>\n    </body>\n  </html>\n";
 var create404Page = function () {
-    var distDirectory = "./dist";
-    var notFoundPagePath = "".concat(distDirectory, "/404.html");
-    // Create the dist directory if it does not exist
-    if (!fs.existsSync(distDirectory)) {
-        fs.mkdirSync(distDirectory);
-    }
-    // Write the 404 HTML content to the 404.html file
-    fs.writeFileSync(notFoundPagePath, htmlContent);
-    console.log("Created ".concat(notFoundPagePath));
+  var distDirectory = "./dist";
+  var notFoundPagePath = "".concat(distDirectory, "/404.html");
+  // Create the dist directory if it does not exist
+  if (!fs.existsSync(distDirectory)) {
+    fs.mkdirSync(distDirectory);
+  }
+  // Write the 404 HTML content to the 404.html file
+  fs.writeFileSync(notFoundPagePath, htmlContent);
+  console.log("Created ".concat(notFoundPagePath));
 };
 create404Page();
